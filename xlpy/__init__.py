0a4aa15d5b74fe69ff1546665e33e4bf75cb8ffb
imking <mrbloo3dz@users.noreply.github.com>
2018-09-01 08:06:01 +0700

    Initial commit

---
+++ xlpy/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by MrBloo3dz'
+__version__         = '1.0.0'
+__author__          = 'Arya (Publisher)'
+__author_email__    = 'aryaheriyan2002@gmail.com'
+__url__             = 'http://github.com/mrbloo3dz/tembak-sc'
+
+__all__ = ['Config','XL']
